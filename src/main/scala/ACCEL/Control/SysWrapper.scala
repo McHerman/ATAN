b992baf48cadc102d723de8db99e74941d86e013
Karl Krause <karl@krauses.dk>
2026-03-30 16:35:11 +0200

    Replaced SysDMA and SysWriteDMA with TLDMA, not fully integrated

--- src/main/scala/ACCEL/Control/SysWrapper.scala
+++ src/main/scala/ACCEL/Control/SysWrapper.scala
@@ -9,7 +9,8 @@ class SysWrapper(implicit c: Configuration) extends Module {
     val scratchOut = Vec(c.grainDim, new TilelinkPort)
     val scratchIn  = Vec(2, Vec(c.grainDim, new TilelinkPort))
 
-    val semaphoreIF = Flipped(new TilelinkPort)
+    val writeSemaphoreIF = Vec(c.grainDim, new TilelinkPort)
+    val readSemaphoreIF = Vec(2, Vec(c.grainDim, new TilelinkPort))
 
     val debug      = new ExeDebug
   })
@@ -17,34 +18,65 @@ class SysWrapper(implicit c: Configuration) extends Module {
   val SysController = Module(new SysController())
   val SysGrain      = Module(new Grain())
 
-  val DMA      = Seq.fill(2, c.grainDim)(Module(new SysDMA()))
-  val WriteDMA = Seq.fill(c.grainDim)(Module(new SysWriteDMA()))
+  //val DMA      = Seq.fill(2, c.grainDim)(Module(new SysDMA()))
+  //val WriteDMA = Seq.fill(c.grainDim)(Module(new SysWriteDMA()))
+
+  val dmaReadConf = TLDMAConfig(read = true, write = false, semaphore = true)
+  val ReadDMA = Seq.fill(2, c.grainDim)(Module(new TLDMA(dmaReadConf)))
+
+  val dmaWriteConf = TLDMAConfig(read = false, write = true, semaphore = true)
+  val WriteDMA = Seq.fill(c.grainDim)(Module(new TLDMA(dmaWriteConf)))
+
 
   SysController.io.in  <> io.in
   SysController.io.out <> SysGrain.io.in
 
-  (SysGrain.io.writePort zip DMA).foreach { case (portVec, dmaVec) =>
-    (portVec zip dmaVec).foreach { case (port, dma) => port <> dma.io.writePort }
+  // ReadDMA dataOut -> Grain writePort (Vec[UInt] <-> flat UInt conversion)
+  (SysGrain.io.writePort zip ReadDMA).foreach { case (portVec, dmaVec) =>
+    (portVec zip dmaVec).foreach { case (port, dma) =>
+      val dataOut = dma.io.dataOut.get
+      port.valid     := dataOut.valid
+      dataOut.ready  := port.ready
+      port.bits      := dataOut.bits.asTypeOf(port.bits)
+    }
   }
 
-  (io.scratchIn.flatten zip DMA.flatten).foreach { case (port, dma) =>
-    dma.io.scratchIn <> port
+  // ReadDMA TileLink ports
+  (io.scratchIn.flatten zip ReadDMA.flatten).foreach { case (port, dma) =>
+    dma.io.tl <> port
   }
 
-  (SysController.io.dmaRead.flatten zip DMA.flatten).foreach { case (command, dma) =>
-    command <> dma.io.in
-  }
+  // TODO: Connect ReadDMA dmaInterface to SysController
+  //(SysController.io.dmaRead.flatten zip ReadDMA.flatten).foreach { case (command, dma) =>
+  //  command <> dma.io.interface
+  //}
 
+  // WriteDMA TileLink ports
   (io.scratchOut zip WriteDMA).foreach { case (port, dma) =>
-    dma.io.scratchOut <> port
+    dma.io.tl <> port
   }
 
-  (SysController.io.dmaWrite zip WriteDMA).foreach { case (command, dma) =>
-    dma.io.in <> command
-  }
+  // TODO: Connect WriteDMA dmaInterface to SysController
+  //(SysController.io.dmaWrite zip WriteDMA).foreach { case (command, dma) =>
+  //  dma.io.interface <> command
+  //}
 
+  // Grain readPort -> WriteDMA dataIn (Vec[UInt] <-> flat UInt conversion)
   (SysGrain.io.readPort zip WriteDMA).foreach { case (port, dma) =>
-    port <> dma.io.readPort
+    val dataIn = dma.io.dataIn.get
+    port.request.ready       := dataIn.request.ready
+    dataIn.request.valid     := port.request.valid
+    dataIn.response.valid    := port.response.valid
+    port.response.bits.readData := dataIn.response.bits.readData.asTypeOf(port.response.bits.readData)
+  }
+
+  // Semaphore interfaces
+  (io.writeSemaphoreIF zip WriteDMA).foreach { case (port, dma) =>
+    dma.io.semaphoreIF.get <> port
+  }
+
+  (io.readSemaphoreIF.flatten zip ReadDMA.flatten).foreach { case (port, dma) =>
+    dma.io.semaphoreIF.get <> port
   }
 
   SysController.io.sysCompleted := SysGrain.io.completed
