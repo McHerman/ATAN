c02a3f4a954e1b29596ba4f940734867b78755b0
Karl Krause <karl@krauses.dk>
2026-05-05 16:30:29 +0200

    Added buffer to LoadController to decouple inputs from tl memory logic

--- src/main/scala/ACCEL/Control/Load/LoadController.scala
+++ src/main/scala/ACCEL/Control/Load/LoadController.scala
@@ -30,8 +30,21 @@ class LoadController(implicit c: Configuration) extends Module {
   ReadDMA.io.tl <> io.writeport
   ReadDMA.io.semaphoreIF.get <> io.semaphoreIF
 
-  io.AXIST.tready := false.B
+  // FIFO between AXIST and DMA dataIn so the AXIST handshake doesn't see
+  // tl.a.ready combinationally through the TileLink xbar.
+  class FifoBeat extends Bundle {
+    val data = UInt(64.W)
+    val last = Bool()
+  }
+  val fifo = Module(new Queue(new FifoBeat, entries = 4, pipe = false, flow = false))
 
+  fifo.io.enq.valid     := io.AXIST.tvalid
+  fifo.io.enq.bits.data := io.AXIST.tdata
+  fifo.io.enq.bits.last := io.AXIST.tlast
+  io.AXIST.tready       := fifo.io.enq.ready
+
+  fifo.io.deq.ready := false.B
+ 
   ReadDMA.io.interface.descriptor.valid := false.B
   ReadDMA.io.interface.descriptor.bits := DontCare
   ReadDMA.io.interface.response.ready := false.B
@@ -48,8 +61,11 @@ class LoadController(implicit c: Configuration) extends Module {
 
   switch(StateReg) {
     is(0.U) { // Fetch instruction
-      when(io.instructionStream.request.ready) {
-        io.instructionStream.request.valid := true.B
+
+     io.instructionStream.request.valid := true.B
+
+
+      when(io.instructionStream.request.fire) {
         when(io.instructionStream.response.valid) {
           reg      := io.instructionStream.response.bits.readData
           StateReg := 1.U
@@ -75,14 +91,13 @@ class LoadController(implicit c: Configuration) extends Module {
         StateReg := 2.U
       }
     }
-    is(2.U) { // Stream AXIST data into DMA, wait for completion
+    is(2.U) { // Drain FIFO into DMA, wait for completion
+      ReadDMA.io.dataIn.get.request.ready          := fifo.io.deq.valid
+      fifo.io.deq.ready                            := ReadDMA.io.dataIn.get.request.valid
+      ReadDMA.io.dataIn.get.response.valid         := fifo.io.deq.valid
+      ReadDMA.io.dataIn.get.response.bits.readData := fifo.io.deq.bits.data
+
       ReadDMA.io.interface.response.ready := true.B
-
-      ReadDMA.io.dataIn.get.request.ready := io.AXIST.tvalid
-      io.AXIST.tready := ReadDMA.io.dataIn.get.request.valid
-
-      ReadDMA.io.dataIn.get.response.valid := io.AXIST.tvalid
-      ReadDMA.io.dataIn.get.response.bits.readData := io.AXIST.tdata
 
       when(ReadDMA.io.interface.response.fire) {
         StateReg := 0.U
