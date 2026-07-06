eddd6c4d1c905ef6dd3b3c4f5291ef39dd222b90
Karl Krause <karl.krause@sonicedge.io>
2026-07-06 09:51:51 +0200

    Added new unified scratchpadhandler module with atomics

---
+++ src/main/scala/ACCEL/ScratchPad/TLScratchpadHandler.scala
@@ -0,0 +1,234 @@
+
+package ATA8
+
+import chisel3._
+import chisel3.util._
+
+case class TLScratchConfig(
+  read: Boolean,
+  write: Boolean,
+  atomic: Boolean,
+) {
+  require(read || write, "Must support at least read or write")
+  require((atomic && read && write) || !atomic, "Must support both read and write when using atomic")
+}
+
+
+object State extends ChiselEnum {
+  val sIdle, sWriteLock, sWriteAck, sReadLock, amoLock, amoRead, amoOp, amoWrite, amoReturn = Value
+}
+
+import State._
+
+class TLScratchpadHandler(config: TLScratchConfig)(implicit c: MemBusConfig) extends Module {
+  val io = IO(new Bundle {
+    val tl   = Flipped(new TilelinkPort)
+    val wMem = if (config.write) Some(Decoupled(new Writeport(
+      new Bundle {
+        val writeData = Vec(c.dataBusSize, UInt(8.W))
+        val strb      = Vec(c.dataBusSize, Bool())
+      }, 16))) else None
+    val rMem = if (config.read) Some(new Readport(Vec(c.dataBusSize, UInt(c.arithDataWidth.W)), Some(16))) else None
+  })
+
+  io.tl.a.ready := false.B
+  io.tl.d.valid := false.B
+  io.tl.d.bits  := DontCare
+  io.wMem.foreach { w => w.valid := false.B; w.bits := DontCare }
+  io.rMem.foreach { r => r.request.valid := false.B; r.request.bits := DontCare }
+
+  val state        = RegInit(sIdle)
+  val beatCnt      = Reg(UInt(24.W))
+  val firstBeat    = RegInit(false.B)
+  val reg          = Reg(new TilelinkA())
+  val originalData = Reg(UInt((c.dataBusSize * 8).W))
+  val amoResult    = Reg(UInt((c.dataBusSize * 8).W))
+
+  when(firstBeat) {
+    io.tl.d.valid        := true.B
+    io.tl.d.bits.opcode  := TilelinkOpcodes.AccessAck
+    io.tl.d.bits.param   := 0.U
+    io.tl.d.bits.size    := reg.size
+    io.tl.d.bits.source  := 0.U
+    io.tl.d.bits.sink    := 0.U
+    io.tl.d.bits.denied  := 0.U
+    io.tl.d.bits.data    := 0.U
+    io.tl.d.bits.corrupt := 0.U
+    when(io.tl.d.fire) { firstBeat := false.B }
+  }
+
+  switch(state) {
+    is(sIdle) {
+      switch(io.tl.a.bits.opcode) {
+        is(TilelinkOpcodes.Get) {
+          assert(config.read.B)
+          if (config.read) {
+            io.tl.a.ready := true.B
+            when(io.tl.a.fire) {
+              reg     := io.tl.a.bits
+              beatCnt := io.tl.a.bits.size - c.dataBusSize.U
+              state   := sReadLock
+            }
+          }
+        }
+        is(TilelinkOpcodes.PutFullData, TilelinkOpcodes.PutPartialData) {
+          assert(config.write.B)
+          if (config.write) {
+            val writeIF = io.wMem.get
+            io.tl.a.ready               := writeIF.ready
+            writeIF.valid                := io.tl.a.valid
+            writeIF.bits.addr            := io.tl.a.bits.address
+            writeIF.bits.data.writeData  := io.tl.a.bits.data.asTypeOf(Vec(c.dataBusSize, UInt(8.W)))
+            writeIF.bits.data.strb       := VecInit(io.tl.a.bits.mask.asBools)
+            when(io.tl.a.fire) {
+              reg := io.tl.a.bits
+              when(io.tl.a.bits.size > c.dataBusSize.U) {
+                reg.address := io.tl.a.bits.address + 1.U
+                beatCnt     := io.tl.a.bits.size - (2 * c.dataBusSize).U
+                state       := sWriteLock
+              }.otherwise {
+                firstBeat := true.B
+              }
+            }
+          }
+        }
+        is(TilelinkOpcodes.ArithmeticData, TilelinkOpcodes.LogicalData) {
+          assert(config.atomic.B)
+          if (config.atomic) {
+            io.tl.a.ready := true.B
+            when(io.tl.a.fire) {
+              reg     := io.tl.a.bits
+              beatCnt := io.tl.a.bits.size - c.dataBusSize.U
+              state   := amoLock
+            }
+          }
+        }
+      }
+    }
+  }
+
+  if (config.write) {
+    val writeIF = io.wMem.get
+    switch(state) {
+      is(sWriteLock) {
+        io.tl.a.ready               := writeIF.ready
+        writeIF.valid                := io.tl.a.valid
+        writeIF.bits.addr            := reg.address
+        writeIF.bits.data.writeData  := io.tl.a.bits.data.asTypeOf(Vec(c.dataBusSize, UInt(8.W)))
+        writeIF.bits.data.strb       := VecInit(io.tl.a.bits.mask.asBools)
+        when(io.tl.a.fire) {
+          reg.address := reg.address + 1.U
+          when(beatCnt === 0.U) {
+            firstBeat := true.B
+            state     := sIdle
+          }.otherwise {
+            beatCnt := beatCnt - c.dataBusSize.U
+          }
+        }
+      }
+    }
+  }
+
+  if (config.read) {
+    val readIF = io.rMem.get
+    switch(state) {
+      is(sReadLock) {
+        readIF.request.valid         := true.B
+        readIF.request.bits.addr.get := reg.address
+        io.tl.d.valid        := readIF.response.valid
+        io.tl.d.bits.opcode  := TilelinkOpcodes.AccessAckData
+        io.tl.d.bits.param   := 0.U
+        io.tl.d.bits.size    := reg.size
+        io.tl.d.bits.source  := 0.U
+        io.tl.d.bits.sink    := 0.U
+        io.tl.d.bits.denied  := 0.U
+        io.tl.d.bits.data    := readIF.response.bits.readData.asUInt
+        io.tl.d.bits.corrupt := 0.U
+        when(readIF.request.fire) { reg.address := reg.address + 1.U }
+        when(io.tl.d.fire) {
+          beatCnt := beatCnt - c.dataBusSize.U
+          when(beatCnt === 0.U) { state := sIdle }
+        }
+      }
+    }
+  }
+
+  if (config.atomic) {
+    val readIF  = io.rMem.get
+    val writeIF = io.wMem.get
+    switch(state) {
+      is(amoLock) {
+        state := amoRead
+      }
+      is(amoRead) {
+        readIF.request.valid         := true.B
+        readIF.request.bits.addr.get := reg.address
+        when(readIF.request.fire) { state := amoOp }
+      }
+      is(amoOp) {
+        when(readIF.response.valid) {
+          val memVal = readIF.response.bits.readData.asUInt
+          val opVal  = reg.data
+          originalData := memVal
+          val result = WireDefault(opVal)
+          switch(reg.opcode) {
+            is(TilelinkOpcodes.ArithmeticData) {
+              switch(reg.param) {
+                is(ArithmeticDataParam.ADD)  { result := memVal + opVal }
+                is(ArithmeticDataParam.MIN)  {
+                  when(memVal.asSInt < opVal.asSInt) { result := memVal }.otherwise { result := opVal }
+                }
+                is(ArithmeticDataParam.MAX)  {
+                  when(memVal.asSInt > opVal.asSInt) { result := memVal }.otherwise { result := opVal }
+                }
+                is(ArithmeticDataParam.MINU) {
+                  when(memVal < opVal) { result := memVal }.otherwise { result := opVal }
+                }
+                is(ArithmeticDataParam.MAXU) {
+                  when(memVal > opVal) { result := memVal }.otherwise { result := opVal }
+                }
+              }
+            }
+            is(TilelinkOpcodes.LogicalData) {
+              switch(reg.param) {
+                is(0.U) { result := memVal ^ opVal }
+                is(1.U) { result := memVal | opVal }
+                is(2.U) { result := memVal & opVal }
+                is(3.U) { result := opVal }
+              }
+            }
+          }
+          amoResult := result
+          state     := amoWrite
+        }
+      }
+      is(amoWrite) {
+        writeIF.valid               := true.B
+        writeIF.bits.addr           := reg.address
+        writeIF.bits.data.writeData := amoResult.asTypeOf(Vec(c.dataBusSize, UInt(8.W)))
+        writeIF.bits.data.strb      := VecInit(Seq.fill(c.dataBusSize)(true.B))
+        when(writeIF.fire) { state := amoReturn }
+      }
+      is(amoReturn) {
+        io.tl.d.valid        := true.B
+        io.tl.d.bits.opcode  := TilelinkOpcodes.AccessAckData
+        io.tl.d.bits.param   := 0.U
+        io.tl.d.bits.size    := reg.size
+        io.tl.d.bits.source  := reg.source
+        io.tl.d.bits.sink    := 0.U
+        io.tl.d.bits.denied  := 0.U
+        io.tl.d.bits.data    := originalData
+        io.tl.d.bits.corrupt := 0.U
+        when(io.tl.d.fire) {
+          when(beatCnt === 0.U) {
+            state := sIdle
+          }.otherwise {
+            reg.address := reg.address + 1.U
+            beatCnt     := beatCnt - c.dataBusSize.U
+            state       := amoLock
+          }
+        }
+      }
+    }
+  }
+}
---
+++ src/test/scala/ACCEL/Scratchpad/TLScratchpadHandlerTest.scala
@@ -0,0 +1,406 @@
+package ATA8
+
+import chisel3._
+import chisel3.simulator.scalatest.ChiselSim
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+
+class TLScratchpadHandlerTest extends AnyFreeSpec with Matchers with ChiselSim {
+
+  val maxCycles = 500
+  implicit val c: Configuration = Configuration.default()
+
+  def waitFor(step: () => Unit)(cond: => Boolean, msg: String): Unit = {
+    var cycles = 0
+    while (!cond) {
+      require(cycles < maxCycles, s"Timeout: $msg (after $maxCycles cycles)")
+      step()
+      cycles += 1
+    }
+  }
+
+  "single-beat PutFullData" in {
+    simulate(new TLScratchpadHandler(TLScratchConfig(read = true, write = true, atomic = true))) { dut =>
+      dut.io.tl.a.valid.poke(false.B)
+      dut.io.tl.d.ready.poke(false.B)
+      dut.io.wMem.get.ready.poke(false.B)
+      dut.io.rMem.get.request.ready.poke(false.B)
+      dut.io.rMem.get.response.valid.poke(false.B)
+
+      dut.io.tl.a.bits.opcode.poke(TilelinkOpcodes.PutFullData)
+      dut.io.tl.a.bits.param.poke(0.U)
+      dut.io.tl.a.bits.size.poke(8.U)
+      dut.io.tl.a.bits.source.poke(0.U)
+      dut.io.tl.a.bits.address.poke(0x0010.U)
+      dut.io.tl.a.bits.mask.poke(0xFF.U)
+      dut.io.tl.a.bits.data.poke(BigInt("0102030405060708", 16).U)
+      dut.io.tl.a.bits.corrupt.poke(0.U)
+      dut.io.tl.a.valid.poke(true.B)
+      dut.io.wMem.get.ready.poke(true.B)
+
+      dut.io.tl.a.ready.expect(true.B)
+      dut.io.wMem.get.valid.expect(true.B)
+      dut.io.wMem.get.bits.addr.expect(0x0010.U)
+
+      dut.clock.step()
+
+      dut.io.tl.a.valid.poke(false.B)
+      dut.io.tl.d.ready.poke(true.B)
+      dut.io.tl.d.valid.expect(true.B)
+      dut.io.tl.d.bits.opcode.expect(TilelinkOpcodes.AccessAck)
+      dut.io.tl.d.bits.size.expect(8.U)
+
+      dut.clock.step()
+      dut.io.tl.d.valid.expect(false.B)
+    }
+  }
+
+  "multi-beat PutFullData write burst" in {
+    simulate(new TLScratchpadHandler(TLScratchConfig(read = true, write = true, atomic = true))) { dut =>
+      dut.io.tl.a.valid.poke(false.B)
+      dut.io.tl.d.ready.poke(false.B)
+      dut.io.wMem.get.ready.poke(false.B)
+      dut.io.rMem.get.request.ready.poke(false.B)
+      dut.io.rMem.get.response.valid.poke(false.B)
+
+      dut.io.tl.a.bits.opcode.poke(TilelinkOpcodes.PutFullData)
+      dut.io.tl.a.bits.param.poke(0.U)
+      dut.io.tl.a.bits.size.poke(16.U)
+      dut.io.tl.a.bits.source.poke(0.U)
+      dut.io.tl.a.bits.address.poke(0x0020.U)
+      dut.io.tl.a.bits.mask.poke(0xFF.U)
+      dut.io.tl.a.bits.data.poke(BigInt("AAAAAAAAAAAAAAAA", 16).U)
+      dut.io.tl.a.bits.corrupt.poke(0.U)
+      dut.io.tl.a.valid.poke(true.B)
+      dut.io.wMem.get.ready.poke(true.B)
+
+      dut.io.tl.a.ready.expect(true.B)
+      dut.io.wMem.get.valid.expect(true.B)
+      dut.io.wMem.get.bits.addr.expect(0x0020.U)
+
+      dut.clock.step()
+
+      // sWriteLock: beat 1 with addrReg=0x0021
+      dut.io.tl.a.bits.data.poke(BigInt("BBBBBBBBBBBBBBBB", 16).U)
+
+      dut.io.tl.a.ready.expect(true.B)
+      dut.io.wMem.get.valid.expect(true.B)
+      dut.io.wMem.get.bits.addr.expect(0x0021.U)
+
+      dut.clock.step()
+
+      dut.io.tl.a.valid.poke(false.B)
+      dut.io.tl.d.ready.poke(true.B)
+      dut.io.tl.d.valid.expect(true.B)
+      dut.io.tl.d.bits.opcode.expect(TilelinkOpcodes.AccessAck)
+      dut.io.tl.d.bits.size.expect(16.U)
+
+      dut.clock.step()
+      dut.io.tl.d.valid.expect(false.B)
+    }
+  }
+
+  "single-beat Get read" in {
+    simulate(new TLScratchpadHandler(TLScratchConfig(read = true, write = true, atomic = true))) { dut =>
+      dut.io.tl.a.valid.poke(false.B)
+      dut.io.tl.d.ready.poke(false.B)
+      dut.io.wMem.get.ready.poke(false.B)
+      dut.io.rMem.get.request.ready.poke(false.B)
+      dut.io.rMem.get.response.valid.poke(false.B)
+
+      dut.io.tl.a.bits.opcode.poke(TilelinkOpcodes.Get)
+      dut.io.tl.a.bits.param.poke(0.U)
+      dut.io.tl.a.bits.size.poke(8.U)
+      dut.io.tl.a.bits.source.poke(0.U)
+      dut.io.tl.a.bits.address.poke(0x0030.U)
+      dut.io.tl.a.bits.mask.poke(0xFF.U)
+      dut.io.tl.a.bits.data.poke(0.U)
+      dut.io.tl.a.bits.corrupt.poke(0.U)
+      dut.io.tl.a.valid.poke(true.B)
+
+      dut.io.tl.a.ready.expect(true.B)
+      dut.clock.step()
+
+      dut.io.tl.a.valid.poke(false.B)
+
+      dut.io.rMem.get.request.valid.expect(true.B)
+      dut.io.rMem.get.request.bits.addr.get.expect(0x0030.U)
+
+      for (i <- 0 until c.dataBusSize) {
+        dut.io.rMem.get.response.bits.readData(i).poke((i + 1).U)
+      }
+      dut.io.rMem.get.response.valid.poke(true.B)
+      dut.io.rMem.get.request.ready.poke(true.B)
+      dut.io.tl.d.ready.poke(true.B)
+
+      dut.io.tl.d.valid.expect(true.B)
+      dut.io.tl.d.bits.opcode.expect(TilelinkOpcodes.AccessAckData)
+      dut.io.tl.d.bits.size.expect(8.U)
+
+      dut.clock.step()
+
+      dut.io.rMem.get.response.valid.poke(false.B)
+      dut.io.tl.d.valid.expect(false.B)
+    }
+  }
+
+  "multi-beat Get read burst" in {
+    simulate(new TLScratchpadHandler(TLScratchConfig(read = true, write = true, atomic = true))) { dut =>
+      dut.io.tl.a.valid.poke(false.B)
+      dut.io.tl.d.ready.poke(false.B)
+      dut.io.wMem.get.ready.poke(false.B)
+      dut.io.rMem.get.request.ready.poke(false.B)
+      dut.io.rMem.get.response.valid.poke(false.B)
+
+      dut.io.tl.a.bits.opcode.poke(TilelinkOpcodes.Get)
+      dut.io.tl.a.bits.param.poke(0.U)
+      dut.io.tl.a.bits.size.poke(16.U)
+      dut.io.tl.a.bits.source.poke(0.U)
+      dut.io.tl.a.bits.address.poke(0x0040.U)
+      dut.io.tl.a.bits.mask.poke(0xFF.U)
+      dut.io.tl.a.bits.data.poke(0.U)
+      dut.io.tl.a.bits.corrupt.poke(0.U)
+      dut.io.tl.a.valid.poke(true.B)
+
+      dut.io.tl.a.ready.expect(true.B)
+      dut.clock.step()
+
+      dut.io.tl.a.valid.poke(false.B)
+      dut.io.rMem.get.request.ready.poke(true.B)
+      dut.io.tl.d.ready.poke(true.B)
+
+      // beat 0: addr=0x0040
+      dut.io.rMem.get.request.valid.expect(true.B)
+      dut.io.rMem.get.request.bits.addr.get.expect(0x0040.U)
+      for (i <- 0 until c.dataBusSize) {
+        dut.io.rMem.get.response.bits.readData(i).poke(0x0A.U)
+      }
+      dut.io.rMem.get.response.valid.poke(true.B)
+
+      dut.io.tl.d.valid.expect(true.B)
+      dut.io.tl.d.bits.opcode.expect(TilelinkOpcodes.AccessAckData)
+
+      dut.clock.step()
+
+      // beat 1: addr=0x0041
+      dut.io.rMem.get.request.valid.expect(true.B)
+      dut.io.rMem.get.request.bits.addr.get.expect(0x0041.U)
+      for (i <- 0 until c.dataBusSize) {
+        dut.io.rMem.get.response.bits.readData(i).poke(0x0B.U)
+      }
+
+      dut.io.tl.d.valid.expect(true.B)
+      dut.io.tl.d.bits.opcode.expect(TilelinkOpcodes.AccessAckData)
+
+      dut.clock.step()
+
+      dut.io.rMem.get.response.valid.poke(false.B)
+      dut.io.tl.d.valid.expect(false.B)
+    }
+  }
+
+  "AMO ADD" in {
+    simulate(new TLScratchpadHandler(TLScratchConfig(read = true, write = true, atomic = true))) { dut =>
+      dut.io.tl.a.valid.poke(false.B)
+      dut.io.tl.d.ready.poke(false.B)
+      dut.io.wMem.get.ready.poke(false.B)
+      dut.io.rMem.get.request.ready.poke(false.B)
+      dut.io.rMem.get.response.valid.poke(false.B)
+
+      dut.io.tl.a.bits.opcode.poke(TilelinkOpcodes.ArithmeticData)
+      dut.io.tl.a.bits.param.poke(ArithmeticDataParam.ADD)
+      dut.io.tl.a.bits.size.poke(8.U)
+      dut.io.tl.a.bits.source.poke(1.U)
+      dut.io.tl.a.bits.address.poke(0x0050.U)
+      dut.io.tl.a.bits.mask.poke(0xFF.U)
+      dut.io.tl.a.bits.data.poke(42.U)
+      dut.io.tl.a.bits.corrupt.poke(0.U)
+      dut.io.tl.a.valid.poke(true.B)
+
+      dut.io.tl.a.ready.expect(true.B)
+      dut.clock.step()   // sIdle: a.fire → amoLock, reg captured
+
+      dut.io.tl.a.valid.poke(false.B)
+      dut.clock.step()   // amoLock → amoRead
+
+      dut.io.rMem.get.request.valid.expect(true.B)
+      dut.io.rMem.get.request.bits.addr.get.expect(0x0050.U)
+      dut.io.rMem.get.request.ready.poke(true.B)
+      dut.clock.step()   // amoRead: request fires → amoOp
+
+      dut.io.rMem.get.request.ready.poke(false.B)
+      dut.io.rMem.get.response.bits.readData(0).poke(100.U)
+      for (i <- 1 until c.dataBusSize) {
+        dut.io.rMem.get.response.bits.readData(i).poke(0.U)
+      }
+      dut.io.rMem.get.response.valid.poke(true.B)
+      dut.clock.step()   // amoOp: memVal=100 + opVal=42 = 142, originalData=100 → amoWrite
+
+      dut.io.rMem.get.response.valid.poke(false.B)
+      dut.io.wMem.get.valid.expect(true.B)
+      dut.io.wMem.get.bits.addr.expect(0x0050.U)
+      dut.io.wMem.get.bits.data.writeData(0).expect(142.U)
+      for (i <- 1 until c.dataBusSize) {
+        dut.io.wMem.get.bits.data.writeData(i).expect(0.U)
+      }
+      dut.io.wMem.get.ready.poke(true.B)
+      dut.clock.step()   // amoWrite: fire → amoReturn
+
+      dut.io.wMem.get.ready.poke(false.B)
+      dut.io.tl.d.valid.expect(true.B)
+      dut.io.tl.d.bits.opcode.expect(TilelinkOpcodes.AccessAckData)
+      dut.io.tl.d.bits.size.expect(8.U)
+      dut.io.tl.d.bits.source.expect(1.U)
+      dut.io.tl.d.bits.data.expect(100.U)
+      dut.io.tl.d.ready.poke(true.B)
+      dut.clock.step()   // amoReturn: d.fire → sIdle
+
+      dut.io.tl.d.valid.expect(false.B)
+    }
+  }
+
+  "AMO SWAP" in {
+    simulate(new TLScratchpadHandler(TLScratchConfig(read = true, write = true, atomic = true))) { dut =>
+      dut.io.tl.a.valid.poke(false.B)
+      dut.io.tl.d.ready.poke(false.B)
+      dut.io.wMem.get.ready.poke(false.B)
+      dut.io.rMem.get.request.ready.poke(false.B)
+      dut.io.rMem.get.response.valid.poke(false.B)
+
+      dut.io.tl.a.bits.opcode.poke(TilelinkOpcodes.LogicalData)
+      dut.io.tl.a.bits.param.poke(3.U)  // SWAP
+      dut.io.tl.a.bits.size.poke(8.U)
+      dut.io.tl.a.bits.source.poke(2.U)
+      dut.io.tl.a.bits.address.poke(0x0060.U)
+      dut.io.tl.a.bits.mask.poke(0xFF.U)
+      dut.io.tl.a.bits.data.poke(0xBEEF.U)
+      dut.io.tl.a.bits.corrupt.poke(0.U)
+      dut.io.tl.a.valid.poke(true.B)
+
+      dut.io.tl.a.ready.expect(true.B)
+      dut.clock.step()   // sIdle: a.fire → amoLock
+
+      dut.io.tl.a.valid.poke(false.B)
+      dut.clock.step()   // amoLock → amoRead
+
+      dut.io.rMem.get.request.valid.expect(true.B)
+      dut.io.rMem.get.request.bits.addr.get.expect(0x0060.U)
+      dut.io.rMem.get.request.ready.poke(true.B)
+      dut.clock.step()   // amoRead: request fires → amoOp
+
+      dut.io.rMem.get.request.ready.poke(false.B)
+      dut.io.rMem.get.response.bits.readData(0).poke(0xAD.U)
+      dut.io.rMem.get.response.bits.readData(1).poke(0xDE.U)
+      for (i <- 2 until c.dataBusSize) {
+        dut.io.rMem.get.response.bits.readData(i).poke(0.U)
+      }
+      dut.io.rMem.get.response.valid.poke(true.B)
+      dut.clock.step()   // amoOp: result=opVal=0xBEEF (SWAP), originalData=0xDEAD → amoWrite
+
+      dut.io.rMem.get.response.valid.poke(false.B)
+      dut.io.wMem.get.valid.expect(true.B)
+      dut.io.wMem.get.bits.addr.expect(0x0060.U)
+      dut.io.wMem.get.bits.data.writeData(0).expect(0xEF.U)
+      dut.io.wMem.get.bits.data.writeData(1).expect(0xBE.U)
+      for (i <- 2 until c.dataBusSize) {
+        dut.io.wMem.get.bits.data.writeData(i).expect(0.U)
+      }
+      dut.io.wMem.get.ready.poke(true.B)
+      dut.clock.step()   // amoWrite: fire → amoReturn
+
+      dut.io.wMem.get.ready.poke(false.B)
+      dut.io.tl.d.valid.expect(true.B)
+      dut.io.tl.d.bits.opcode.expect(TilelinkOpcodes.AccessAckData)
+      dut.io.tl.d.bits.data.expect(0xDEAD.U)
+      dut.io.tl.d.ready.poke(true.B)
+      dut.clock.step()   // amoReturn: d.fire → sIdle
+
+      dut.io.tl.d.valid.expect(false.B)
+    }
+  }
+
+  "multi-beat AMO ADD burst" in {
+    simulate(new TLScratchpadHandler(TLScratchConfig(read = true, write = true, atomic = true))) { dut =>
+      dut.io.tl.a.valid.poke(false.B)
+      dut.io.tl.d.ready.poke(false.B)
+      dut.io.wMem.get.ready.poke(false.B)
+      dut.io.rMem.get.request.ready.poke(false.B)
+      dut.io.rMem.get.response.valid.poke(false.B)
+
+      dut.io.tl.a.bits.opcode.poke(TilelinkOpcodes.ArithmeticData)
+      dut.io.tl.a.bits.param.poke(ArithmeticDataParam.ADD)
+      dut.io.tl.a.bits.size.poke(16.U)
+      dut.io.tl.a.bits.source.poke(0.U)
+      dut.io.tl.a.bits.address.poke(0x0070.U)
+      dut.io.tl.a.bits.mask.poke(0xFF.U)
+      dut.io.tl.a.bits.data.poke(10.U)
+      dut.io.tl.a.bits.corrupt.poke(0.U)
+      dut.io.tl.a.valid.poke(true.B)
+
+      dut.io.tl.a.ready.expect(true.B)
+      dut.clock.step()   // sIdle: a.fire → amoLock, beatCnt=8
+
+      dut.io.tl.a.valid.poke(false.B)
+      dut.clock.step()   // amoLock → amoRead
+
+      // beat 0: addr=0x0070, memVal=100, result=110
+      dut.io.rMem.get.request.valid.expect(true.B)
+      dut.io.rMem.get.request.bits.addr.get.expect(0x0070.U)
+      dut.io.rMem.get.request.ready.poke(true.B)
+      dut.clock.step()   // amoRead: request fires → amoOp
+
+      dut.io.rMem.get.request.ready.poke(false.B)
+      dut.io.rMem.get.response.bits.readData(0).poke(100.U)
+      for (i <- 1 until c.dataBusSize) { dut.io.rMem.get.response.bits.readData(i).poke(0.U) }
+      dut.io.rMem.get.response.valid.poke(true.B)
+      dut.clock.step()   // amoOp: 100+10=110 → amoWrite
+
+      dut.io.rMem.get.response.valid.poke(false.B)
+      dut.io.wMem.get.valid.expect(true.B)
+      dut.io.wMem.get.bits.addr.expect(0x0070.U)
+      dut.io.wMem.get.bits.data.writeData(0).expect(110.U)
+      for (i <- 1 until c.dataBusSize) { dut.io.wMem.get.bits.data.writeData(i).expect(0.U) }
+      dut.io.wMem.get.ready.poke(true.B)
+      dut.clock.step()   // amoWrite: fire → amoReturn
+
+      dut.io.wMem.get.ready.poke(false.B)
+      dut.io.tl.d.valid.expect(true.B)
+      dut.io.tl.d.bits.opcode.expect(TilelinkOpcodes.AccessAckData)
+      dut.io.tl.d.bits.data.expect(100.U)
+      dut.io.tl.d.ready.poke(true.B)
+      dut.clock.step()   // amoReturn: d.fire, beatCnt>0 → amoLock, addr=0x0071, beatCnt=0
+
+      // beat 1: addr=0x0071, memVal=200, result=210
+      dut.io.tl.d.ready.poke(false.B)
+      dut.clock.step()   // amoLock → amoRead
+
+      dut.io.rMem.get.request.valid.expect(true.B)
+      dut.io.rMem.get.request.bits.addr.get.expect(0x0071.U)
+      dut.io.rMem.get.request.ready.poke(true.B)
+      dut.clock.step()   // amoRead: request fires → amoOp
+
+      dut.io.rMem.get.request.ready.poke(false.B)
+      dut.io.rMem.get.response.bits.readData(0).poke(200.U)
+      for (i <- 1 until c.dataBusSize) { dut.io.rMem.get.response.bits.readData(i).poke(0.U) }
+      dut.io.rMem.get.response.valid.poke(true.B)
+      dut.clock.step()   // amoOp: 200+10=210 → amoWrite
+
+      dut.io.rMem.get.response.valid.poke(false.B)
+      dut.io.wMem.get.valid.expect(true.B)
+      dut.io.wMem.get.bits.addr.expect(0x0071.U)
+      dut.io.wMem.get.bits.data.writeData(0).expect(210.U)
+      for (i <- 1 until c.dataBusSize) { dut.io.wMem.get.bits.data.writeData(i).expect(0.U) }
+      dut.io.wMem.get.ready.poke(true.B)
+      dut.clock.step()   // amoWrite: fire → amoReturn
+
+      dut.io.wMem.get.ready.poke(false.B)
+      dut.io.tl.d.valid.expect(true.B)
+      dut.io.tl.d.bits.opcode.expect(TilelinkOpcodes.AccessAckData)
+      dut.io.tl.d.bits.data.expect(200.U)
+      dut.io.tl.d.ready.poke(true.B)
+      dut.clock.step()   // amoReturn: d.fire, beatCnt=0 → sIdle
+
+      dut.io.tl.d.valid.expect(false.B)
+    }
+  }
+}
